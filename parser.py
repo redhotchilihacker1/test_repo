be0fe064ef071fa42ad55cb209bec066d6cc42a1
redhotchilihacker1 <72512209+redhotchilihacker1@users.noreply.github.com>
2024-10-07 16:28:04 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -21,7 +21,7 @@ with open(output_csv_file, 'w', newline='', encoding='utf-8') as csvfile:
     vulnerabilities_found = False
     for row in soup.find_all('tr'):
         columns = row.find_all('td')
-        
+
         if len(columns) >= 4:  # Eğer yeterli sütun varsa
             # Zafiyet başlığını bulma
             title_elem = columns[0].find(attrs={'onmouseover': True})
@@ -32,12 +32,21 @@ with open(output_csv_file, 'w', newline='', encoding='utf-8') as csvfile:
 
             # Risk Faktörü
             risk_factor_elem = row.find(string="Risk Factor")
-            risk_factor = risk_factor_elem.find_next('td').get_text(strip=True) if risk_factor_elem else None
+            if risk_factor_elem:
+                risk_factor = risk_factor_elem.find_next('td').get_text(strip=True)
+            else:
+                risk_factor = None
             
             # CVSS Skoru
-            cvss_score_elem = row.find(string=["CVSS v2.0 Base Score", "CVSS v3.0 Base Score"])
-            cvss_score = cvss_score_elem.find_next('td').get_text(strip=True) if cvss_score_elem else None
-            
+            cvss_score_elem_v2 = row.find(string="CVSS v2.0 Base Score")
+            cvss_score_elem_v3 = row.find(string="CVSS v3.0 Base Score")
+
+            cvss_score = None
+            if cvss_score_elem_v2:
+                cvss_score = cvss_score_elem_v2.find_next('td').get_text(strip=True)
+            elif cvss_score_elem_v3:
+                cvss_score = cvss_score_elem_v3.find_next('td').get_text(strip=True)
+
             # CSV'ye yaz
             if title and ip_port and risk_factor and cvss_score:
                 writer.writerow({
